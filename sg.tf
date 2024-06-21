23f6be239e06ec103e9bd199b158d5cb3d31bcb9
“[Benard <ngwabern@gmail.com>
2024-06-21 00:48:51 -0500

    Security group created, user data added to ec2

---
+++ sg.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "PCE-SG" {
+  name        = "PCE-SG"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.Ben.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Egress rules
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # -1 means all protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "PCE-SG"
+  }
+}
+
+
+
