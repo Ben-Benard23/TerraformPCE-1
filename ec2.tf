c693087f6796fb1dd23744d50549557363f2c94a
“[Benard <ngwabern@gmail.com>
2024-07-07 19:41:54 -0500

    instance profile added

--- ec2.tf
+++ ec2.tf
@@ -12,3 +12,30 @@ resource "aws_instance" "Ben" {
   count = 2
 
 }
+
+resource "aws_iam_role" "admin_role" {
+  name = "admin_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "admin_role_policy_attachment" {
+  role       = aws_iam_role.admin_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_instance_profile" "admin_profile" {
+  name = "admin_profile"
+  role = aws_iam_role.admin_role.name
+}
